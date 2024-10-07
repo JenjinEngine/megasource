b42bb871a9557f14207487d5902ca6723d1444e5
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-10-07 14:39:46 +0100

    Remove dependencies that are unnecessary for Jenjin

---
+++ README.md
@@ -0,0 +1,24 @@
+Megasource is a CMake-buildable collection of all Jenjin's dependencies from [LÖVE][https://love2d.org].
+
+Prerequisites
+-------------
+ - [CMake 3.5][cmake].
+ - [Visual Studio 2022][Visual Studio] (or another CMake-compatible compiler).
+
+Building
+--------
+To build just the dependencies for 64 bit x86 using Visual Studio 2022:
+```sh
+	$ git clone https://github.com/JenjinEngine/megasource megasource
+	$ cd megasource
+	$ cmake -G Ninja -A x64 -S . -B build
+	$ cmake --build build --config Release
+```
+
+[Jenjin]: https://github.com/JenjinEngine
+[Love2D]: http://love2d.org
+[CMake]: http://www.cmake.org/
+[NSIS]: http://nsis.sourceforge.net
+[Visual Studio]: https://visualstudio.microsoft.com/downloads/
+[Generators]: https://cmake.org/cmake/help/latest/manual/cmake-generators.7.html#visual-studio-generators
+[Architectures]: https://cmake.org/cmake/help/latest/generator/Visual%20Studio%2016%202019.html#platform-selection
--- src/test.cpp
+++ src/test.cpp
@@ -1,26 +1,12 @@
-
-#include <vector>
+#include <functional>
 #include <iostream>
 #include <sstream>
-#include <functional>
+#include <vector>
 
-#include <zlib.h>
-#include <ogg/ogg.h>
-#include <vorbis/codec.h>
-#include <vorbis/vorbisfile.h>
-#include <freetype/config/ftconfig.h>
-#include <freetype/freetype.h>
-#include <SDL.h>
 #include <AL/al.h>
 #include <AL/alext.h>
-#include <modplug.h>
 
-extern "C" {
-#	include "lua.h"
-}
-
-typedef std::stringstream strs;
-typedef std::function<std::string (strs &, strs &)> vfunc;
+#include "lua.hpp"
 
 std::string pad(std::string s, size_t size = 16)
 {
@@ -30,116 +16,34 @@ std::string pad(std::string s, size_t size = 16)
 	return s;
 }
 
-int main(int argc, char **argv)
-{
-	vfunc zlib = [](strs &c, strs &l)
-	{
-		c << ZLIB_VERSION;
-		l << zlibVersion();
-		return "zlib";
-	};
-
-	vfunc lua = [](strs &c, strs &l)
-	{
+int main(int argc, char **argv) {
+	auto lua = [](std::stringstream &compOut) {
 		std::string compiled = LUA_RELEASE;
 
 		compiled = compiled.substr(4);
 
-		c << compiled;
-		l << "N/A";
+		compOut << compiled;
 		return "Lua";
 	};
 
-	vfunc ogg = [](strs &c, strs &l)
-	{
-		c << "N/A";
-		l << "N/A";
-		return "ogg";
-	};
-
-	vfunc vorbis = [](strs &c, strs &l)
-	{
-		c << "N/A";
-		l << "N/A";
-		return "vorbis";
-	};
-
-	vfunc vorbisfile = [](strs &c, strs &l)
-	{
-		c << "N/A";
-		l << "N/A";
-		return "vorbisfile";
-	};
-
-	vfunc freetype = [](strs &c, strs &l)
-	{
-		FT_Library lib;
-		FT_Init_FreeType(&lib);
-		FT_Int major, minor, patch;
-		FT_Library_Version(lib, &major, &minor, &patch);
-		c << "N/A";
-		l << major << "." << minor << "." << patch;
-		return "freetype";
-	};
-
-	vfunc SDL = [](strs &c, strs &l)
-	{
-#if SDL_VERSION_ATLEAST(3, 0, 0)
-		SDL_Version compiled;
-		SDL_Version linked;
-#else
-		SDL_version compiled;
-		SDL_version linked;
-#endif
-
-		SDL_VERSION(&compiled);
-		SDL_GetVersion(&linked);
-
-		c << (int)compiled.major << "." << (int)compiled.minor << "." << (int)compiled.patch;
-		l << (int)linked.major << "." << (int)linked.minor << "." << (int)linked.patch;
-#if SDL_VERSION_ATLEAST(3, 0, 0)
-		return "SDL3";
-#else
-		return "SDL2";
-#endif
-	};
-
-	vfunc OpenAL = [](strs &c, strs &l)
-	{
+	auto OpenAL = [](std::stringstream &compOut) {
 		alIsEnabled(AL_SOURCE_DISTANCE_MODEL);
 
-		c << "N/A";
-		l << "N/A";
+		compOut << "N/A";
 		return "OpenAL";
 	};
 
-	vfunc modplug = [](strs &c, strs &l)
-	{
-		ModPlug_Settings settings;
-		ModPlug_GetSettings(&settings);
-		c << "N/A";
-		l << "N/A";
-		return "modplug";
-	};
-
-	std::vector<vfunc> funcs;
-	funcs.push_back(zlib);
+	std::vector<std::function<std::string(std::stringstream&)>> funcs;
 	funcs.push_back(lua);
-	funcs.push_back(ogg);
-	funcs.push_back(vorbis);
-	funcs.push_back(vorbisfile);
-	funcs.push_back(freetype);
-	funcs.push_back(SDL);
 	funcs.push_back(OpenAL);
-	funcs.push_back(modplug);
 
-	for (size_t i = 0; i < funcs.size(); ++i)
-	{
-		vfunc f = funcs[i];
-		strs c, l;
-		std::string name = f(c, l);
-		std::cout << "-- " << pad(name) << "   compiled: " << pad(c.str(), 7) << "   linked: " << pad(l.str(), 7) << std::endl;
+	for (auto& f : funcs) {
+		std::stringstream c;
+		std::string name = f(c);
+
+		std::cout << "-- " << pad(name) << "   compiled: " << pad(c.str(), 7) << std::endl;
 	}
 
-	return getchar();
+	std::cout << "Press RETURN to exit..." << std::endl;
+	getchar(); return 0;
 }
